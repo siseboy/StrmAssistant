686e9890eba863b8ea6109342fafa2dfa56d707c
OneManJS <onemanjs@github.com>
2025-12-02 21:20:23 +0800

    修复强制媒体排序，修改版本号2.1.0.1

--- StrmAssistant/Mod/EnforceLibraryOrder.cs
+++ StrmAssistant/Mod/EnforceLibraryOrder.cs
@@ -1,6 +1,9 @@
-﻿using HarmonyLib;
+using HarmonyLib;
 using MediaBrowser.Controller.Entities;
+using MediaBrowser.Controller.Library;
+using MediaBrowser.Model.Library;
 using StrmAssistant.Common;
+using System;
 using System.Linq;
 using System.Reflection;
 using static StrmAssistant.Mod.PatchManager;
@@ -10,6 +13,7 @@ namespace StrmAssistant.Mod
     public class EnforceLibraryOrder : PatchBase<EnforceLibraryOrder>
     {
         private static MethodInfo _getUserViews;
+        private static IUserManager _userManager;
 
         public EnforceLibraryOrder()
         {
@@ -29,6 +33,9 @@ namespace StrmAssistant.Mod
             _getUserViews = userViewManager.GetMethods(BindingFlags.Instance | BindingFlags.Public)
                 .FirstOrDefault(m => m.Name == "GetUserViews" &&
                                      (m.GetParameters().Length == 3 || m.GetParameters().Length == 4));
+            
+            // 获取 UserManager 以便在 Prefix 中使用
+            _userManager = Plugin.Instance.ApplicationHost.Resolve<IUserManager>();
         }
 
         protected override void Prepare(bool apply)
@@ -37,9 +44,32 @@ namespace StrmAssistant.Mod
         }
 
         [HarmonyPrefix]
-        private static bool GetUserViewsPrefix(User user)
+        private static bool GetUserViewsPrefix(UserViewQuery query)
         {
-            user.Configuration.OrderedViews = LibraryApi.AdminOrderedViews;
+            try
+            {
+                // 从 query 中获取 UserId
+                var userIdProperty = query.GetType().GetProperty("UserId");
+                if (userIdProperty != null)
+                {
+                    var userId = userIdProperty.GetValue(query) as string;
+                    if (!string.IsNullOrEmpty(userId) && _userManager != null)
+                    {
+                        var user = _userManager.GetUserById(userId);
+                        if (user != null)
+                        {
+                            user.Configuration.OrderedViews = LibraryApi.AdminOrderedViews;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                if (Plugin.Instance.DebugMode)
+                {
+                    Plugin.Instance.Logger.Debug($"EnforceLibraryOrder GetUserViewsPrefix error: {ex.Message}");
+                }
+            }
 
             return true;
         }
